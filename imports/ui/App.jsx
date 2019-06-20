e284ba753cbd227cf84d14ca07cb54d2e5bbf74c
menelike <mitschnacker@gmx.net>
2019-06-20 08:54:57 +0200

    use weakmap to detect dependency change and then use reactive function instead of possible stale (tracker)state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,29 @@
 import {createBrowserHistory} from 'history';
 import {Route, Router, Switch} from 'react-router-dom';
 import React, {useState, useEffect} from 'react';
-import { Tracker } from 'meteor/tracker';
-import { withTracker } from 'meteor/react-meteor-data';
+import {Tracker} from 'meteor/tracker';
+import {withTracker} from 'meteor/react-meteor-data';
+import {Meteor} from 'meteor/meteor';
 
 const history = createBrowserHistory();
+
+const trackerInstances = {};
+let instanceCount = 0;
+let depCount = 0;
+
+const depId = (instanceId, object) => {
+  if (!(instanceId in trackerInstances)) {
+    trackerInstances[instanceId] = new WeakMap();
+  }
+
+  const depIdMap = trackerInstances[instanceId];
+
+  if (!depIdMap.has(instanceId, object)) {
+    depIdMap.set(object, ++depCount);
+  }
+
+  return depIdMap.get(object);
+};
 
 const useTracker = (reactiveFn, deps = []) => {
   // Note : we always run the reactiveFn in Tracker.nonreactive in case
@@ -13,41 +32,51 @@ const useTracker = (reactiveFn, deps = []) => {
   // of the normal behavior of nested Computations, where if the outer one is
   // invalidated or stopped, it stops the inner one too.
 
-  const [trackerData, setTrackerData] = useState(() => {
+  const [[trackerData, nameSpace, oldDepsId], setTrackerData] = useState(() => {
     // No side-effects are allowed when computing the initial value.
     // To get the initial return value for the 1st render on mount,
     // we run reactiveFn without autorun or subscriptions.
     // Note: maybe when React Suspense is officially available we could
     // throw a Promise instead to skip the 1st render altogether ?
-    return reactiveFn();
+    const instanceId = ++instanceCount;
+    const realSubscribe = Meteor.subscribe;
+    Meteor.subscribe = () => ({
+      stop: () => {
+      }, ready: () => false
+    });
+    const initialData = [Tracker.nonreactive(reactiveFn), instanceId, depId(instanceId, deps)];
+    Meteor.subscribe = realSubscribe;
+    return initialData;
   });
 
   useEffect(() => {
     // Set up the reactive computation.
-    const data = reactiveFn();
-    setTrackerData(data);
-    // Set up the reactive computation.
     const computation = Tracker.nonreactive(() =>
       Tracker.autorun(() => {
         const data = reactiveFn();
-        setTrackerData(data);
+        setTrackerData([data, nameSpace, depId(nameSpace, deps)]);
       })
     );
     // On effect cleanup, stop the computation.
-    return () => computation.stop();
+    return () => {
+      delete trackerInstances[nameSpace];
+      computation.stop();
+    }
   }, deps);
 
-  if (reactiveFn() !== trackerData) {
-    console.error(`tracker missmatch, expected ${reactiveFn()} got ${trackerData}`)
+  const data = oldDepsId === depId(nameSpace, deps) ? trackerData : reactiveFn();
+
+  if (reactiveFn() !== data) {
+    console.error(`tracker missmatch, expected ${reactiveFn()} got ${data}`)
   }
 
-  return trackerData;
+  return data;
 };
 
 function withTrackerNew(options) {
   return Component => {
-    const expandedOptions = typeof options === 'function' ? { getMeteorData: options } : options;
-    const { getMeteorData, pure = true } = expandedOptions;
+    const expandedOptions = typeof options === 'function' ? {getMeteorData: options} : options;
+    const {getMeteorData, pure = true} = expandedOptions;
 
     const WithTracker = React.forwardRef((props, ref) => {
       const data = useTracker(() => getMeteorData(props) || {}, [props]);
@@ -127,15 +156,15 @@ const App = () => (
       </button>
       <br/>
       <Switch>
-        <RouteWrapper exact path="/" render={() => <span>/</span>} />
-        <RouteWrapper path="/hookFoo" render={() => <span>/hookFoo</span>} />
-        <RouteWrapper path="/hookBar" render={() => <span>/hookBar</span>} />
-        <NewHOCRouteWrapper path="/newWithTrackerFoo" render={() => <span>/newWithTrackerFoo</span>} />
-        <NewHOCRouteWrapper path="/newWithTrackerBar" render={() => <span>/newWithTrackerBar</span>} />
-        <HOCRouteWrapper path="/withTrackerFoo" render={() => <span>/withTrackerFoo</span>} />
-        <HOCRouteWrapper path="/withTrackerBar" render={() => <span>/withTrackerBar</span>} />
-        <RouteWrapper key="keyedfoo" path="/keyedfoo" render={() => <span>/keyedfoo</span>} />
-        <RouteWrapper key="keyedbar" path="/keyedbar" render={() => <span>/keyedbar</span>} />
+        <RouteWrapper exact path="/" render={() => <span>/</span>}/>
+        <RouteWrapper path="/hookFoo" render={() => <span>/hookFoo</span>}/>
+        <RouteWrapper path="/hookBar" render={() => <span>/hookBar</span>}/>
+        <NewHOCRouteWrapper path="/newWithTrackerFoo" render={() => <span>/newWithTrackerFoo</span>}/>
+        <NewHOCRouteWrapper path="/newWithTrackerBar" render={() => <span>/newWithTrackerBar</span>}/>
+        <HOCRouteWrapper path="/withTrackerFoo" render={() => <span>/withTrackerFoo</span>}/>
+        <HOCRouteWrapper path="/withTrackerBar" render={() => <span>/withTrackerBar</span>}/>
+        <RouteWrapper key="keyedfoo" path="/keyedfoo" render={() => <span>/keyedfoo</span>}/>
+        <RouteWrapper key="keyedbar" path="/keyedbar" render={() => <span>/keyedbar</span>}/>
       </Switch>
     </React.Fragment>
   </Router>
